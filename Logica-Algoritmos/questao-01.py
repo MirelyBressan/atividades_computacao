bbac806135dc5fcb2180a3dab6901674d26e0e1b
mirely.bressan <mirely.bd@gmail.com>
2024-12-11 15:40:32 -0300

    01

--- Logica-Algoritmos/questao-01.py
+++ Logica-Algoritmos/questao-01.py
@@ -1,27 +1,31 @@
-#Saída nome do desenvolvedor:
+#Saída do nome do desenvolvedor:
 
-print("Bem vindo ao Sistema da Mirely Bressan!")
+print("Bem-vindos ao sistema desenvolvido por Mirely Bressan!")
 
-#Receber os valores do plano e idade:
+#Solicitação das informações de valor base e idade:
 
-valorBase = float(input("Informe o valor base do plano: R$ "))
-idade = int(input("Informe a idade do cliente: "))
+valor_base = float(input("Digite o valor base do plano em reais: R$ "))
+idade = int(input("Digite a idade do cliente: "))
 
-#Calculo da porcentagem de acordo com a idade:
+#Calculo das porcentagens de acordo com a idade:
 
-if 0 <=idade< 19:
-    porcentagem = 100/100
-elif 19 <=idade<29:
-    porcentagem = 150/100
-elif 29 <=idade< 39:
-    porcentagem = 225/100
-elif 39 <=idade< 49:
-    porcentagem = 240/100
-elif 49 <=idade< 59:
-    porcentagem = 350/100
+if idade < 19:
+  porcentagem = 100/100
+elif idade  < 29:
+  porcentagem = 150/100
+elif idade < 39:
+  porcentagem = 225/100
+elif idade < 49:
+  porcentagem = 240/100
+elif idade < 59:
+  porcentagem = 350/100
 else:
-    porcentagem = 600/100
+  porcentagem = 600/100
 
-valorMensal = valorBase * porcentagem
+#Calculo do valor mensal do plano:
 
-print(f"O valor do Plano Mensal para {idade} anos é: R$ {valorMensal:.2f}")
+valor_mensal = valor_base * porcentagem
+
+#Saída do resultado do plano mensal:
+
+print(f"O valor mensal do plano para a idade {idade} anos é de: R$ {valor_mensal:.2f}")
